6197765542e29892ed6b6f21eeeef8725ac333ed
tgxhx <tgxhx2020@gmail.com>
2019-05-17 10:56:47 +0800

    build doc

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,35 @@ const path = require('path');
 const url = require('url');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const dev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  mode: 'development',
-  devtool: 'sourcemap',
+  mode: dev ? 'development' : 'production',
+  devtool: dev ? 'sourcemap' : false,
   entry: {
     app: path.resolve(__dirname, 'docs/src'),
   },
   output: {
-    filename: '[name].js',
+    filename: `[name]${dev ? '' : '.[contenthash:10]'}.js`,
+    path: path.resolve(__dirname, 'docs'),
+    publicPath: !dev ? '/' : '/react-image-zooms',
   },
   resolve: {
     extensions: ['.js', '.ts', '.tsx'],
+  },
+  optimization: {
+    minimize: dev ? false : true,
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+        terserOptions: { output: { comments: false } },
+      }),
+    ],
   },
   module: {
     rules: [
@@ -38,7 +55,18 @@ module.exports = {
       },
       {
         test: /.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader'],
+        use: [
+          dev
+            ? 'style-loader'
+            : {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: '',
+                },
+              },
+          'css-loader',
+          'less-loader',
+        ],
       },
     ],
   },
@@ -50,6 +78,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'docs/src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: `[name]${dev ? '' : '.[contenthash:10]'}.css`,
     }),
   ],
 };
